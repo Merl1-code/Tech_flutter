6891dc486b9076fe8a25db137115ce500f1c87a4
Ludovic Vasseur <ludovic.vasseur@epitech.eu>
2020-11-08 17:00:42 +0100

    Update profile.dart

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -76,128 +76,130 @@ class _ProfilState extends State<Profil> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: theme.spacings.bodyPadding,
-      child: Center(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              Center(
-                child: _image == null
-                    ? const Text('No image selected.')
-                    : Image.file(_image),
-              ),
-              Button(
-                onPressed: getImage, text: 'photo',
-              ),
-              FittedBox(
-                child: Text(
-                  'Modify Email',
-                  style: theme.texts.subtitle,
+    return SingleChildScrollView(
+      child: Container(
+        padding: theme.spacings.bodyPadding,
+        child: Center(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Center(
+                  child: _image == null
+                      ? const Text('No image selected.')
+                      : Image.file(_image),
                 ),
-              ),
-              const SizedBox(height: 10.0),
-              Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    EmailField(
-                      controller: emailController,
-                      textColor: Colors.white,
-                      backgroundColor: theme.colors.background,
-                      validator: (String value) {
-                        if (value.isEmpty && passwordController.value.text.isEmpty) {
-                          return 'Please enter an email';
-                        } else if (!RegExp(
-                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                            .hasMatch(value) && value.isNotEmpty) {
-                          return 'Email is incorrect';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 15.0),
-                    EmailField(
-                      controller: verifEmailController,
-                      validator: (String value) {
-                        if (value != emailController.value.text) {
-                          return 'Emails doesn\'t match';
-                        }
-                        return null;
-                      },
-                      hint: 'Confirm Email',
-                      textColor: Colors.white,
-                      backgroundColor: theme.colors.background,
-                    ),
-                    const SizedBox(height: 25.0),
-                    FittedBox(
-                      child: Text(
-                        'Modify password',
-                        style: theme.texts.subtitle,
+                Button(
+                  onPressed: getImage, text: 'photo',
+                ),
+                FittedBox(
+                  child: Text(
+                    'Modify Email',
+                    style: theme.texts.subtitle,
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      EmailField(
+                        controller: emailController,
+                        textColor: Colors.white,
+                        backgroundColor: theme.colors.background,
+                        validator: (String value) {
+                          if (value.isEmpty && passwordController.value.text.isEmpty) {
+                            return 'Please enter an email';
+                          } else if (!RegExp(
+                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                              .hasMatch(value) && value.isNotEmpty) {
+                            return 'Email is incorrect';
+                          }
+                          return null;
+                        },
                       ),
-                    ),
-                    const SizedBox(height: 10.0),
-                    PasswordField(
-                      controller: passwordController,
-                      textColor: Colors.white,
-                      backgroundColor: theme.colors.background,
-                      validator: (String value) {
-                        if (value.isEmpty && emailController.value.text.isEmpty) {
-                          return 'Please enter a valid password';
-                        } else if (value.length < 8 && value.isNotEmpty) {
-                          return 'Password should be at least 8 characters';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 15.0),
-                    PasswordField(
-                      controller: verifPasswordController,
-                      validator: (String value) {
-                        if (value != passwordController.value.text) {
-                          return 'Password doesn\'t math';
-                        }
-                        return null;
-                      },
-                      hint: 'Confirm password',
-                      textColor: Colors.white,
-                      backgroundColor: theme.colors.background,
-                    ),
-                    const SizedBox(height: 15.0),
-                    PrimaryButton(
-                      text: 'Modify',
-                      onPressed: _validateForm,
-                    ),
-                    Button(
-                      text: 'Logout',
-                      onPressed: () async {
-                        await logout();
-                        Navigator.popUntil(
-                          context,
-                          ModalRoute.withName('/'),
-                        );
-                      },
-                      backgroundColor: theme.colors.background,
-                      textColor: Colors.white,
-                    ),
-                    const SizedBox(height: 15.0),
-                    Text(modifyEmailString,
-                      style: const TextStyle(
-                        fontSize: 12,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
-                    ),
-                    Text(modifyPasswordString,
-                      style: const TextStyle(
+                      const SizedBox(height: 15.0),
+                      EmailField(
+                        controller: verifEmailController,
+                        validator: (String value) {
+                          if (value != emailController.value.text) {
+                            return 'Emails doesn\'t match';
+                          }
+                          return null;
+                        },
+                        hint: 'Confirm Email',
+                        textColor: Colors.white,
+                        backgroundColor: theme.colors.background,
+                      ),
+                      const SizedBox(height: 25.0),
+                      FittedBox(
+                        child: Text(
+                          'Modify password',
+                          style: theme.texts.subtitle,
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      PasswordField(
+                        controller: passwordController,
+                        textColor: Colors.white,
+                        backgroundColor: theme.colors.background,
+                        validator: (String value) {
+                          if (value.isEmpty && emailController.value.text.isEmpty) {
+                            return 'Please enter a valid password';
+                          } else if (value.length < 8 && value.isNotEmpty) {
+                            return 'Password should be at least 8 characters';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 15.0),
+                      PasswordField(
+                        controller: verifPasswordController,
+                        validator: (String value) {
+                          if (value != passwordController.value.text) {
+                            return 'Password doesn\'t math';
+                          }
+                          return null;
+                        },
+                        hint: 'Confirm password',
+                        textColor: Colors.white,
+                        backgroundColor: theme.colors.background,
+                      ),
+                      const SizedBox(height: 15.0),
+                      PrimaryButton(
+                        text: 'Modify',
+                        onPressed: _validateForm,
+                      ),
+                      Button(
+                        text: 'Logout',
+                        onPressed: () async {
+                          await logout();
+                          Navigator.popUntil(
+                            context,
+                            ModalRoute.withName('/'),
+                          );
+                        },
+                        backgroundColor: theme.colors.background,
+                        textColor: Colors.white,
+                      ),
+                      const SizedBox(height: 15.0),
+                      Text(modifyEmailString,
+                        style: const TextStyle(
                           fontSize: 12,
                           color: Colors.white,
                           fontWeight: FontWeight.bold),
-                    )
-                  ],
+                      ),
+                      Text(modifyPasswordString,
+                        style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
